a949c0f42233c7e725de52bc3a73c6796d42b3c6
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-14 18:02:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 
 def reverse_each_word(sentence)
-  new_array = sentence.split(" ")
-  nn_array= []
-  new_array.each do |word| word .reverse
-  nn_array << word.reverse
+  array = sentence.split(" ")
+  new_array= []
+  array.each do |word|
+  new_array << word.reverse
   end
-  nn_array()
+  new_array.join()
 end 
